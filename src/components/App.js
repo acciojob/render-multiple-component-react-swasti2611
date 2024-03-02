169d7ac47227eac0a128aa484590de68fc1cb3fb
Swati jagtap <swatijagtap5060@gmail.com>
2024-03-02 16:13:38 +0530

    Assignment Submitted

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,12 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <div className="ns-wrapper">
+      <h1 data-ns-test="project-name">My Projeect</h1>
+      <h6 data-ns-test="project-description">This a description of my project.</h6>
+       </div>
+    </div>
   )
 }
 
